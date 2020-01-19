2bbc6eeabff2a3e8468ced5662b7dac532badc5b
abecus <insaaone@gmail.com>
2020-01-19 22:07:45 +0530

    .

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
 	long_description_content_type='text/markdown',
 	name = 'pyqueues',         # How you named your package folder (MyLib)
 	packages = ['pyqueues'],   # Chose the same as "name"
-	version = '1.1',      # Start with a small number and increase it with every change you make
+	version = '1.2',      # Start with a small number and increase it with every change you make
 	license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
 	description = 'A library for Priory-Queues containing Indexed-Priority-Queue and Simple Priority-Queue',   # Give a short description about your library
 	author = 'Abdul Salam',                   # Type in your name
